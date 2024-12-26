d06fc1b26dfe5c0d70e8a8658a6671b10d03e1a7
ClemouDouard <clementleveque911@gmail.com>
2024-12-26 15:19:14 +0100

    added dataset test

--- src/lin_reg.py
+++ src/lin_reg.py
@@ -3,9 +3,14 @@ import numpy as np
 
 class Lin_reg:
 
-    def __init__(self, df: pd.DataFrame, target: str, step: float):
+    def __init__(self, df: pd.DataFrame, target: str, step: float) -> None:
+        self.target = np.array(df[target])
+        self.init_coef(df)
+        self.step = step
+        self.values = np.array(df.drop(columns=[target]))
+
+    def init_coef(self, df: pd.DataFrame) -> None:
         self.coef = np.array([])
-        self.coef = np.append(self.coef, np.mean(df[target]))
+        self.coef = np.append(self.coef, np.mean(self.target))
         for i in range (1, df.shape[1]):
             self.coef = np.append(self.coef, 0)
-        self.step = step
--- src/main.py
+++ src/main.py
@@ -1,1 +1,7 @@
 from lin_reg import Lin_reg
+import pandas as pd
+
+if __name__ == "__main__":
+    df = pd.read_csv("../ProjWindTurbine.txt", sep=",")
+    my_linear_regression = Lin_reg(df, "POW", 2)
+    print(my_linear_regression.coef)
