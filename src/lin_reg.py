69c0b089466568d5c673ad8c7a2c9a1ba0075d6e
ClemouDouard <clementleveque911@gmail.com>
2024-12-26 15:59:01 +0100

    added grad function

--- src/lin_reg.py
+++ src/lin_reg.py
@@ -5,16 +5,17 @@ class Lin_reg:
 
     def __init__(self, df: pd.DataFrame, target: str, step: float) -> None:
         self.target = np.array(df[target])
-        self.init_coef(df)
+        self.init_coef()
         self.step = step
         self.values = np.array(df.drop(columns=[target]))
         self.values = np.c_[np.ones(self.values.shape[0]), self.values]
         self.n = df.shape[0]
+        self.p = df.shape[1]
 
-    def init_coef(self, df: pd.DataFrame) -> None:
+    def init_coef(self) -> None:
         self.coef = np.array([])
         self.coef = np.append(self.coef, np.mean(self.target))
-        for i in range (1, df.shape[1]):
+        for i in range (1, self.p):
             self.coef = np.append(self.coef, 0)
 
     def predict(self) -> float:
@@ -22,3 +23,9 @@ class Lin_reg:
 
     def MSE(self) -> float:
         return 1/self.n * float(np.sum(self.target - self.predict()))**2
+
+    def grad(self) -> np.ndarray:
+        grad = np.array([])
+        for i in range (self.p):
+            grad = np.append(grad, -2/self.n * float(np.sum(np.multiply(self.values[:, i], self.target - self.predict()))))
+        return grad
