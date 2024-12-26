20ccb4d91f9977f050679828f59b3d348ec70fec
ClemouDouard <clementleveque911@gmail.com>
2024-12-26 15:49:26 +0100

    working mse

--- src/lin_reg.py
+++ src/lin_reg.py
@@ -8,9 +8,17 @@ class Lin_reg:
         self.init_coef(df)
         self.step = step
         self.values = np.array(df.drop(columns=[target]))
+        self.values = np.c_[np.ones(self.values.shape[0]), self.values]
+        self.n = df.shape[0]
 
     def init_coef(self, df: pd.DataFrame) -> None:
         self.coef = np.array([])
         self.coef = np.append(self.coef, np.mean(self.target))
         for i in range (1, df.shape[1]):
             self.coef = np.append(self.coef, 0)
+
+    def predict(self) -> float:
+        return np.dot(self.values, self.coef)
+
+    def MSE(self) -> float:
+        return 1/self.n * float(np.sum(self.target - self.predict()))**2
--- src/main.py
+++ src/main.py
@@ -4,4 +4,5 @@ import pandas as pd
 if __name__ == "__main__":
     df = pd.read_csv("../ProjWindTurbine.txt", sep=",")
     my_linear_regression = Lin_reg(df, "POW", 2)
-    print(my_linear_regression.coef)
+    print(my_linear_regression.target)
+    print(my_linear_regression.predict())
